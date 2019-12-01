e1316a861d2fa2281a934089eb05712fd7576886
EvenYi <yiyangmagi@gmail.com>
2019-11-30 19:35:01 -0500

    data preprocess for star distribution

---
+++ Datapreprocess.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import re
+
+
+def star_distribution(csv_filename='StarDistribution.csv'):
+    data = pd.read_csv(csv_filename)
+    data_list = data.values.tolist()
+    error_data = []
+    for each in data_list:
+        if len(each[1].split("%")) == 6:
+            each[1] = each[1].split("%")
+            if each[1].pop() == "":
+                each[1] = [float(i) for i in each[1]]
+
+            else:
+                error_data.append(each[0])
+        else:
+            error_data.append(each[0])
+    return data_list
