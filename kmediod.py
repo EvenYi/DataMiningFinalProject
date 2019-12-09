1e9a76fa64ba8e84c9f385f18d4d69fee40c4653
EvenYi <yiyangmagi@gmail.com>
2019-12-08 23:08:13 -0500

    update kmediod.py

--- kmediod.py
+++ kmediod.py
@@ -14,29 +14,57 @@ pure_data = []
 for each in data_set:
     pure_data.append(each[1])
 sample = pure_data
-print(sample)
 # Initial medoids for sample 'Simple3'.
-initial_medoids = [52, 456, 6973, 614]
+initial_medoids = [4358,10, 13, 456, 536, 3229,614]
 
 
 # Create instance of K-Medoids algorithm with prepared centers.
+'''
 def my_cosine(point1, point2):
     result = 1 - spatial.distance.cosine(point1, point2)
     return result
+'''
 
-
-metric = distance_metric(type_metric.USER_DEFINED, func=my_cosine)
-kmedoids_instance = kmedoids(sample, initial_medoids, metric=metric)
+#metric = distance_metric(type_metric.USER_DEFINED, func=my_cosine)
+#metric = distance_metric(type_metric.MANHATTAN, degree=5)
+kmedoids_instance = kmedoids(sample, initial_medoids)
 # Run cluster analysis.
 kmedoids_instance.process()
 # Calculate the closest cluster to following two points.
 # points = [[0.35, 0.5], [2.5, 2.0]]
 # closest_clusters = kmedoids_instance.predict(points)
 clusters = kmedoids_instance.get_clusters()
-for each in clusters:
-    for item in each:
-        print(data_set[item], end="")
-    print("----------------------")
+for item in clusters[0]:
+    print(data_set[item], item, end="")
+print(" ")
+print(" ")
+for item in clusters[1]:
+    print(data_set[item], item, end="")
+print(" ")
+print(" ")
+for item in clusters[2]:
+    print(data_set[item], item, end="")
+print(" ")
+print(" ")
+for item in clusters[3]:
+    print(data_set[item], item, end="")
+print(" ")
+print(" ")
+for item in clusters[4]:
+    print(data_set[item], item, end="")
+print(" ")
+print(" ")
+for item in clusters[5]:
+    print(data_set[item], item, end="")
+print(" ")
+print(" ")
+for item in clusters[6]:
+    print(data_set[item], item, end="")
+print(" ")
+print(" ")
+
+
+
 # print(closest_clusters)
 # visualizer = cluster_visualizer_multidim();
 # visualizer.append_clusters(clusters, sample);
